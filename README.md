c1b45c0e84e6d41eb22c4758c8efe834a1dd9004
dediane <ddecourt@student.42.fr>
2023-01-19 13:14:05 +0100

    minor update

--- README.md
+++ README.md
@@ -16,3 +16,10 @@ npm run start:dev
 Swagger:
 
 http://localhost:8000/api
+
+voir les pages:
+
+http://localhost:8000 -> Backend
+http://localhost:3000 -> Frontend
+
+Before starting your webapp locally don t forget to add the .env file.
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,23 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController, ProfileController } from './controllers';
 import { AppService } from './services/app.service';
 import { EventsGateway } from './gateways/socket';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 @Module({
-  imports: [ConfigModule, ConfigModule.forRoot({ envFilePath: ['.env'] })],
+  imports: [
+    ConfigModule,
+    ConfigModule.forRoot({ envFilePath: ['.env'] }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: process.env.POSTGRES_USER,
+    //   password: process.env.POSTGRES_PASSWORD,
+    //   database: process.env.POSTGRES_DB,
+    //   entities: [],
+    //   synchronize: true,
+    // })
+  ],
   controllers: [AppController, ProfileController],
   providers: [AppService, EventsGateway],
 })
