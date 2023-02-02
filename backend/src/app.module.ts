925b3e848822570cb47143a85089701442c77272
dediane <ddecourt@student.42.fr>
2023-02-02 23:36:25 +0100

    try to setup the DB

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,24 +1,26 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController, ProfileController } from './controllers';
 import { AppService } from './services/app.service';
 import { EventsGateway } from './gateways/socket';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigService } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+import { DatabaseModule } from './database.module';
+
 @Module({
   imports: [
     ConfigModule,
-    ConfigModule.forRoot({ envFilePath: ['.env'] }),
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   host: 'localhost',
-    //   port: 3306,
-    //   username: process.env.POSTGRES_USER,
-    //   password: process.env.POSTGRES_PASSWORD,
-    //   database: process.env.POSTGRES_DB,
-    //   entities: [],
-    //   synchronize: true,
-    // })
+    ConfigModule.forRoot({ 
+      envFilePath: ['.env'] ,
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+      })
+    }),
+    DatabaseModule,
   ],
   controllers: [AppController, ProfileController],
   providers: [AppService, EventsGateway],
