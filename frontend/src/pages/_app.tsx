afcafc7ef0263a5c642b0ba4b3cfdafbae23f5ce
Diane <dianedecourt@gmail.com>
2023-02-22 15:35:38 +0100

    update and demo navbar on going

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from '@/components/Layout';
+
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <div>
+      <Layout>
       <Component {...pageProps} />
+      </Layout>
     </div>
   );
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -8,20 +8,24 @@ import { useRouter } from "next/router";
 import Modal from '@/components/Modal'
 import React, { useState, useEffect } from 'react';
 import { Authentication } from './login'
+import { Navbar } from '@/components/Navbar'
 
 
 function Home() {
   const [showModal, setShowModal] = useState(false);
   return (
-    <div className='mx-auto text-2xl text-center'>Welcome to our Transcendance!
-      <button onClick={() => setShowModal(true)} className='bg-red-400 px-6 py-2 rounded-2xl'>
-        Modal
-      </button>
-      <Modal show={showModal} view={<Authentication/>} onClose={() => setShowModal(false)}>
-      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </Modal>
+    <div>
     </div>
   )
 }
 export default Home
 
+
+{/* <div className='mx-auto text-2xl text-center'>Welcome to our Transcendance!
+  <button onClick={() => setShowModal(true)} className='bg-red-400 px-6 py-2 rounded-2xl'>
+    Modal
+  </button>
+  <Modal show={showModal} view={<Authentication/>} onClose={() => setShowModal(false)}>
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+  </Modal>
+</div> */}
