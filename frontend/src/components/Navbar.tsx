e809957e84c791f1a735de0435a76cd4cbead6d1
Diane <dianedecourt@gmail.com>
2023-02-22 15:53:00 +0100

    adding navbar for routing

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,10 +6,6 @@ export const Navbar = () => {
     <nav className={styles.navbar}>
     <div className="container flex flex-wrap items-center justify-between mx-auto">
     <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Ft_Transcendence</span>
-    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
-      <span className="sr-only">Open main menu</span>
-      <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-    </button>
     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
         <li>
@@ -27,7 +23,6 @@ export const Navbar = () => {
           <a href="#" className={styles.link}>
             Login</a>
         </li>
-       
       </ul>
     </div>
   </div>
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout';
+import Home from '.';
 
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <div>
-      <Layout>
+      <Layout {...pageProps}>
       <Component {...pageProps} />
       </Layout>
     </div>
