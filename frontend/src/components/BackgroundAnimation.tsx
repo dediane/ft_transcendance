38977023a95d3dea9390ee0fae09769da030791a
balkisous <balkisous@gmail.com>
2023-05-28 11:08:38 +0200

    background change

--- frontend/src/components/BackgroundAnimation.tsx
+++ frontend/src/components/BackgroundAnimation.tsx
@@ -1,4 +1,23 @@
 import dynamic from "next/dynamic";
 import GradientCanvas from "../components/GradientCanvas";
 
-export const BackgroundAnimation = dynamic(() => import("../components/GradientCanvas"), { ssr: false });
+export const BackgroundAnimation = () => {
+    return (
+      <div
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          background: 'linear-gradient(to bottom, #e7c0ff, #c5edff)',
+          zIndex: -1,
+        }}
+      ></div>
+    );
+  };
+  //#efcdfe
+  //#d1c8ff
+  // up clair
+  //#dad6ff
+  //#8d2bd2
