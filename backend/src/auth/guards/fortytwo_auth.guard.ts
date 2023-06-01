32e3f8c065f2466ca0069a6c14f935975fcd52f1
Diane <dianedecourt@gmail.com>
2023-06-01 02:24:48 +0200

    Auth fix 2FA 42oauth

--- backend/src/auth/guards/fortytwo_auth.guard.ts
+++ backend/src/auth/guards/fortytwo_auth.guard.ts
@@ -2,4 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class FortyTwoAuthGuard extends AuthGuard('42') {}
+export class FortyTwoAuthGuard extends AuthGuard('42') {
+  handleRequest(err: any, user: any, info: any, context: any, status?: any) {
+    const request = context.switchToHttp().getRequest();
+
+    if (request.query.error) {
+      // Redirect to login page if error exists in query params
+      return request.res.redirect('http://localhost:3000/login');
+    }
+    if (!user)
+        return
+    return super.handleRequest(err, user, info, context, status);
+  }
+}
