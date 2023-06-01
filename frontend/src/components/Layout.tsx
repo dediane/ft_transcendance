f10bee64e8505bdb354d1f30d530153a12211191
Diane <dianedecourt@gmail.com>
2023-06-01 02:27:01 +0200

    disable navbar, public profile no auth

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -9,8 +9,8 @@ export default function Layout({ children } : {children: any}) {
   return (
     <>
       {/* <div id='layoutDiv'> */}
-      {/* {pathname !== '/login' && <Navbar />} */}
-      <Navbar/>
+      {pathname !== '/login' && <Navbar />}
+      {/* <Navbar/> */}
       <main>{children}</main>
       {/* <main style={{flex:1, overflow: children.displayName === "Game" ? "hidden" : undefined}}>{children}</main> */}
       {/* </div> */}
--- frontend/src/pages/public.tsx
+++ frontend/src/pages/public.tsx
@@ -6,12 +6,21 @@ import { useRouter } from "next/router";
 import { LayoutGroupContext } from "framer-motion";
 import _, { remove } from "lodash";
 import { PublicProfil } from "@/components/PublicProfile";
-
+import authServiceInstance from "@/services/authentication-service";
 export default function Homepage() {
+    const [loading, setLoading] = useState(false)
+    const router = useRouter()
+    useEffect(()=>{
+        if(!authServiceInstance.isAuthentificated()) {
+            router.push('/login')
+        } else {
+            setLoading(true)
+        }
+    }, [])
     return (
         <>
         <div className={styles.container}>
-            <PublicProfil />
+           {loading && <PublicProfil />}
             
         </div>
         </>
