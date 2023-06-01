444eaa84c43e4e17e3c9eb87648689347d894807
Diane <dianedecourt@gmail.com>
2023-06-01 02:36:53 +0200

    remove anoying console.log

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -6,7 +6,6 @@ export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone()   
   let isLogged = request.cookies.get("isLogged")
   if (url.pathname !== "/login" && !isLogged) {
-    console.log("redirect to login")
     return NextResponse.redirect(new URL('/login', request.url));
   } else if (url.pathname == "/login" && isLogged) {
     console.log("redirect to profile")
