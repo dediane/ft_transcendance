8501bc5c09331a480c42bec38d6820a0c14fb758
balkisous <balkisous@gmail.com>
2023-06-01 11:56:49 +0200

    hahah here i am

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -5,15 +5,15 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone()   
   let isLogged = request.cookies.get("isLogged")
-  if (url.pathname !== "/login" && !isLogged) {
-    // console.log("redirect to login")
-    return NextResponse.redirect(new URL('/login', request.url));
-  } else if (url.pathname == "/login" && isLogged) {
-    // console.log("redirect to profile")
-    return NextResponse.redirect(new URL('/profile', request.url));
-  } else {
-    return NextResponse.next()
-  }
+  // if (url.pathname !== "/login" && !isLogged) {
+  //   // console.log("redirect to login")
+  //   return NextResponse.redirect(new URL('/login', request.url));
+  // } else if (url.pathname == "/login" && isLogged) {
+  //   // console.log("redirect to profile")
+  //   return NextResponse.redirect(new URL('/profile', request.url));
+  // } else {
+  //   return NextResponse.next()
+  // }
 }
 
 export const config = {
